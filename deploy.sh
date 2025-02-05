74c81b2191242c5b88e17eb4262ccf476abd5189
Sachin sonawane <83231163+sachin012sp@users.noreply.github.com>
2025-02-05 18:23:11 +0530

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:Lidyaboshra/my-Portfolio.git master:gh-pages
+git push -f git@github.com:sachin012sp/my-Portfolio.git master:gh-pages
 
 cd -
